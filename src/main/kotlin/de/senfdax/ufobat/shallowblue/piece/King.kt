e06dfb52e05cc5200cee3b93061f6ba95cf0c770
Martin Barth <mbarth@netdescribe.com>
2022-03-31 21:59:19 +0200

    added King Moves

--- src/main/kotlin/de/senfdax/ufobat/shallowblue/piece/King.kt
+++ src/main/kotlin/de/senfdax/ufobat/shallowblue/piece/King.kt
@@ -3,21 +3,29 @@ package de.senfdax.ufobat.shallowblue.piece
 import de.senfdax.ufobat.shallowblue.Move
 import de.senfdax.ufobat.shallowblue.Piece
 import de.senfdax.ufobat.shallowblue.Position
+import de.senfdax.ufobat.shallowblue.move.CastleMove
 import de.senfdax.ufobat.shallowblue.move.SimpleMove
 
 class King(position: Position) : Piece(position) {
-    override fun pseudoLegalMoves(): List<SimpleMove> {
+    override fun pseudoLegalMoves(): List<Move> {
         val positions = (-1..1).flatMap { x ->
             (-1 .. 1).map { y ->
                 if (y!=0 || x != 0)
-                    Position.fromCoordinates(position.row + x, position.col + y)
+                    Position.fromCoordinates(position.col + x, position.row + y)
                 else
                     null
             }
         }
 
-        return positions
+        val simpleMoves =  positions
             .filterNotNull()
             .map { SimpleMove(position, it) }
+
+        val castleMoves =
+            if (position == Position.e1) {
+                listOf(CastleMove(Position.a1), CastleMove(Position.h1))
+            } else listOf()
+
+        return simpleMoves + castleMoves
     }
 }
--- src/test/kotlin/de/senfdax/ufobat/shallowblue/piece/KingTest.kt
+++ src/test/kotlin/de/senfdax/ufobat/shallowblue/piece/KingTest.kt
@@ -1,6 +1,7 @@
 package de.senfdax.ufobat.shallowblue.piece
 
 import de.senfdax.ufobat.shallowblue.Position
+import de.senfdax.ufobat.shallowblue.move.CastleMove
 import de.senfdax.ufobat.shallowblue.move.SimpleMove
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -18,6 +19,23 @@ class KingTest {
                 SimpleMove(Position.a1, Position.b2),
             ),
             moves.toSet()
+        )
+    }
+
+    @Test
+    fun moveKingWithCastling() {
+        val king = King(Position.e1)
+        assertEquals(
+            setOf(
+                SimpleMove(Position.e1, Position.d1),
+                SimpleMove(Position.e1, Position.d2),
+                SimpleMove(Position.e1, Position.e2),
+                SimpleMove(Position.e1, Position.f1),
+                SimpleMove(Position.e1, Position.f2),
+                CastleMove(Position.a1),
+                CastleMove(Position.h1),
+            ),
+            king.pseudoLegalMoves().toSet()
         )
     }
 }
