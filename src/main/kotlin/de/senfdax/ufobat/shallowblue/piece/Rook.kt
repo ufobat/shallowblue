04a4356fa8ee3bb106c722cceaff73e1b7cfee53
Martin Barth <martin@senfdax.de>
2022-04-01 10:40:53 +0200

    rook and bishop moves

--- src/main/kotlin/de/senfdax/ufobat/shallowblue/piece/Rook.kt
+++ src/main/kotlin/de/senfdax/ufobat/shallowblue/piece/Rook.kt
@@ -3,9 +3,14 @@ package de.senfdax.ufobat.shallowblue.piece
 import de.senfdax.ufobat.shallowblue.Move
 import de.senfdax.ufobat.shallowblue.Piece
 import de.senfdax.ufobat.shallowblue.Position
+import de.senfdax.ufobat.shallowblue.move.SimpleMove
 
 class Rook(position: Position) : Piece(position) {
-        override fun pseudoLegalMoves(): List<Move> {
-        TODO("Not yet implemented")
+    override fun pseudoLegalMoves(): List<Move> {
+        return (0..7)
+            .flatMap { listOf(Position.fromCoordinates(it, position.row), Position.fromCoordinates(position.col, it)) }
+            .filterNotNull()
+            .filterNot { it == position }
+            .map { SimpleMove(position, it) }
     }
 }
